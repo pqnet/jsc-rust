fdcd59ce69b04acb15a2a7cfa02dbbb6a704e402
Leonardo Bartoloni <leonardo@biobeats.com>
2020-07-05 18:47:38 +0200

    JSValue equality

--- jsc/src/jsvalue.rs
+++ jsc/src/jsvalue.rs
@@ -10,6 +10,7 @@ pub struct JSValue {
 impl Drop for JSValue {
   fn drop(&mut self) {
     unsafe {
+      // safeguard against uninitialized values (e.g. exception buffers)
       if !self.ptr.is_null() {
         jsc::JSValueUnprotect(self.ctx.raw(), self.ptr);
         self.ptr = ptr::null();
@@ -32,5 +33,31 @@ impl JSValue {
   }
   pub fn raw(&self) -> JSValueRef {
     self.ptr
+  }
+  pub fn loosely_equal(&self, other: &JSValue) -> Result<bool, JSValue> {
+    if self.ctx.raw() != other.ctx.raw() {
+      panic!("cannot compare values from two different contexts")
+    } else {
+      let mut exception = unsafe { JSValue::from_raw(self.ctx.clone(), ptr::null()) };
+      let result =
+        unsafe { jsc::JSValueIsEqual(self.ctx.raw(), self.ptr, other.ptr, &mut exception.ptr) };
+      if exception.ptr.is_null() {
+        Ok(result)
+      } else {
+        Err(exception)
+      }
+    }
+  }
+  pub fn equal(&self, other: &JSValue) -> bool {
+    if self.ctx.raw() != other.ctx.raw() {
+      panic!("cannot compare values from two different contexts")
+    } else {
+      unsafe { jsc::JSValueIsStrictEqual(self.ctx.raw(), self.ptr, other.ptr) }
+    }
+  }
+}
+impl PartialEq for JSValue {
+  fn eq(&self, other: &JSValue) -> bool {
+    self.equal(other)
   }
 }
